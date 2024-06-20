bf633196c128c884f9601515279ea901deeb7c0f
Luuk Visser <l.f.visser@zoetermeer.nl>
2024-06-20 14:51:04 +0200

    add more aliases

--- .shell_aliases/cli.sh
+++ .shell_aliases/cli.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 alias ..='cd ..'
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+# Enable aliases to be sudo’ed
+alias sudo='sudo '
 
 shellrc=$HOME/.$(basename $SHELL)rc
 alias reshell="source $shellrc"
